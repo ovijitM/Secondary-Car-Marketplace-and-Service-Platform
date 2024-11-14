de4ac1345269b3b655988e1534fdf0c8c746c79b
mdrakibhossain719 <141365102+mdrakibhossain719@users.noreply.github.com>
2024-11-15 03:59:23 +0600

    update

--- src/components/Customnavbar.jsx
+++ src/components/Customnavbar.jsx
@@ -1,23 +1,50 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-
-export default function CustomNavbar() {
+function NavScrollExample() {
   return (
-    <>
-       <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">DriveHub</Navbar.Brand>
-          <Nav className="mx-auto">
-            <Nav.Link href="#features" className='px-4'>Cars</Nav.Link>
-            <Nav.Link href="#Rent" className='px-4'>Rent</Nav.Link>
-            <Nav.Link href="#Repair" className='px-4'>Repair</Nav.Link>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Dr.Driving</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Buy</Nav.Link>
+            <Nav.Link href="#action2">Rent</Nav.Link>
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Rent</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            <Nav.Link href="#">Sell</Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    </>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-
+export default NavScrollExample;
